90cfe00492ce72ea21224b8ef14178f854e2e735
jarrodwatts <jarrod@thirdweb.com>
2024-09-13 18:35:28 +1000

    fix type issue

--- app/page.tsx
+++ app/page.tsx
@@ -61,6 +61,7 @@ export default function Home() {
 
         <TabsContent value="deploy-smart-wallet">
           <DeploySmartWallet
+            // @ts-ignore
             privateKeyHex={privateKey ? toHex(privateKey.privateKey) : null}
           />
         </TabsContent>
